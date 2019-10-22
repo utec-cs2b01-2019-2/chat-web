cf1f70c58bb89258e538b437e62fe8ef25ab62a0
jesubellido <jesubellido@gmail.com>
2019-10-22 12:20:48 -0500

    heroku deploy

--- web/static/js/users.js
+++ web/static/js/users.js
@@ -1,5 +1,5 @@
 $(function(){
-    var url = "/users";
+    var url = "http://127.0.0.1:8000/users";
     $("#grid").dxDataGrid({
         dataSource: DevExpress.data.AspNet.createStore({
             key: "id",
